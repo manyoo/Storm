132a2cb3c2e5fe851de10d47440e1536dee0902f
Eric Wong <ericsyw@gmail.com>
2013-05-13 15:18:25 +0800

    Complete SODServer exception handling

--- lib/Storm/Base/sodserver.rb
+++ lib/Storm/Base/sodserver.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'excon'
+require 'Storm/Base/exception'
 
 module Storm
   module Base
@@ -9,33 +10,46 @@ module Storm
     class SODServer
       def self.remote_call(path, paramter={})
         real_path = "/#{STORM_API_VERSION}#{path}"
-        resp = Excon.post :path => real_path, :body => paramter.to_json
+        param = {}
+        param[:params] = parameter
+        resp = Excon.post :path => real_path, :body => param.to_json
         if resp.status == 200
-          resp_data = JSON.parse resp.body
-          # TODO: Check response exception here.
+          data = JSON.parse resp.body
+          if data[:error_class]
+            e_msg = "#{ data[:error_class] } : #{ data[:error_message] }"
+            raise StormException, e_msg
+          else
+            data
+          end
         else
-          # TODO: Raise customized Exception here.
+          raise HttpException, "HTTP Error: #{ resp.status.to_s }"
         end
       end
 
       def self.remote_list(path, parameter={})
         real_path = "/#{STORM_API_VERSION}#{path}"
-        resp = Excon.post :path => real_path, :body => paramter.to_json
+        param = {}
+        param[:params] = parameter
+        resp = Excon.post :path => real_path, :body => param.to_json
         if resp.status == 200
-          resp_data = JSON.parse resp.body
-          # TODO: Check response exception here.
-
-          res = {}
-          res[:items_count] = data[:items_count]
-          res[:items_total] = data[:items_total]
-          res[:page_num] = data[:page_num]
-          res[:page_size] = data[:page_size]
-          res[:page_total] = data[:page_total]
-          res[:items] = data[:items].map { |e| yield e }
-          res
+          data = JSON.parse resp.body
+          if data[:error_class]
+            e_msg = "#{ data[:error_class] } : #{ data[:error_message] }"
+            raise StormException, e_msg
+          else
+            res = {}
+            res[:items_count] = data[:items_count]
+            res[:items_total] = data[:items_total]
+            res[:page_num] = data[:page_num]
+            res[:page_size] = data[:page_size]
+            res[:page_total] = data[:page_total]
+            res[:items] = data[:items].map { |e| yield e }
+            res
+          end
         else
-          # TODO: Raise customized Exception here.
+          raise HttpException, "HTTP Error: #{ resp.status.to_s }"
         end
+      end
     end
   end
 end
